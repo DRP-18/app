b2b94098408dbec6c8daa8fa6fb3acd6cccef9c0
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-06-09 11:32:09 +0100

    Added mock functionality to calendar bloc

--- tutor_me/lib/bloc/calendar.dart
+++ tutor_me/lib/bloc/calendar.dart
@@ -4,20 +4,28 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class CalendarBloc extends Bloc<CalendarEvent, List<Task>> {
   CalendarBloc(List<Task> initialState) : super(initialState);
 
-  List<Task> _state = List.empty();
+  final List<Task> _currentState = List.empty();
 
-  List<Task> get initialState => _state;
-
+  List<Task> get initialState => _currentState;
 
   @override
   Stream<List<Task>> mapEventToState(CalendarEvent event) async* {
-    yield event.handle(_state);
+      yield event.handle(_currentState);
   }
-
 }
 
-class Task{
+class Task {
+  late int? id;
+  late DateTime start;
+  late DateTime end;
+  late String content;
 
+  Task(DateTime start, DateTime end, String content, [int? id]) {
+    this.start = start;
+    this.end = end;
+    this.content = content;
+    this.id = id;
+  }
 }
 
 abstract class CalendarEvent {
@@ -25,15 +33,30 @@ abstract class CalendarEvent {
 }
 
 class Add extends CalendarEvent {
+  late Task _task;
+
+  Add(Task task) {
+    _task = task;
+  }
+
   @override
   List<Task> handle(List<Task> state) {
+    state.add(_task);
     return state;
   }
 }
 
 class Remove extends CalendarEvent {
+
+  late int _id;
+
+  Remove(int id) {
+    _id = id;
+  }
+
   @override
   List<Task> handle(List<Task> state) {
+    state.removeWhere((element) => element.id == _id);
     return state;
   }
 }
