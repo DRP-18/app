1d1824f68377b2b46729f77f443b82e0f16d37c2
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-06-16 11:50:07 +0100

    Converted task.dart to use abstract dataBloc class

--- tutor_me/lib/bloc/task.dart
+++ tutor_me/lib/bloc/task.dart
@@ -1,31 +1,27 @@
-import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'dart:async';
 import 'package:intl/intl.dart';
+import 'data_bloc.dart';
 
-class TaskBloc extends Bloc<TaskEvent, List<Task>> {
-  final String userID;
-  final String tuteeID;
+class TaskBloc extends DataBloc<Task> {
+  final String _userID;
+  final String _tuteeID;
 
-  TaskBloc(this.userID, this.tuteeID) : super([]);
-
-  List<Task> get initialState => [];
+  TaskBloc(this._userID, this._tuteeID) : super([_userID, _tuteeID]);
 
   @override
-  Stream<List<Task>> mapEventToState(TaskEvent event) async* {
+  Stream<List<Task>> mapEventToState(DataEvent<Task> event) async* {
     switch (event.runtimeType) {
-      case Add:
-        var add = event as Add;
+      case AddTask:
+        var add = event as AddTask;
         yield this.state + [add._task];
         break;
-      case Remove:
-        var rem = event as Remove;
+      case RemoveTask:
+        var rem = event as RemoveTask;
         yield this.state.where((element) => element.id != rem._id).toList();
         break;
-      case Done:
-        var done = event as Done;
+      case DoneTask:
+        var done = event as DoneTask;
         yield this
             .state
             .map((task) => task.id == done._id
@@ -36,7 +32,7 @@ class TaskBloc extends Bloc<TaskEvent, List<Task>> {
       default:
         break;
     }
-    yield await event.handle(userID, tuteeID);
+    super.mapEventToState(event);
   }
 }
 
@@ -54,26 +50,91 @@ class Task {
     this.id = id;
     this.done = done;
   }
+
+  static Task fromJson(Map obj) {
+    return Task(
+      DateTime.parse(obj["startTime"]!),
+      DateTime.parse(obj["endTime"]!),
+      obj["content"]!,
+      obj["done"]!,
+      obj["id"]!,
+    );
+  }
 }
 
-abstract class TaskEvent {
-  Future<List<Task>> handle(String userID, String tuteeID);
+class RefreshTask extends Refresh<Task> {
+  final _url = Uri.parse("https://tutor-drp.herokuapp.com/tuteetasks");
+  final String? _tuteeName;
+
+  RefreshTask(this._tuteeName) : super((v) => Task.fromJson(v));
+
+  @override
+  Future<http.Response> request(List<String> fields) {
+    final tuteeID = fields[1];
+    return http.get(_url, headers: {
+      "Cookie":
+          "user_id=$tuteeID${_tuteeName == null ? "" : ";tutee_name=$_tuteeName"}"
+    });
+  }
 }
 
-class Add extends TaskEvent {
+class DoneTask extends DataEvent<Task> {
+  final url = Uri.parse("https://tutor-drp.herokuapp.com/donetask");
+  late int _id;
+
+  DoneTask(int id) {
+    this._id = id;
+  }
+
+  Future<List<Task>> handle(List<String> fields) async {
+    final userID = fields[0];
+    await http.post(url, headers: {
+      "Cookie": "user_id=$userID",
+    }, body: {
+      "task_id": "$_id",
+    });
+    return RefreshTask("Mika").handle(fields);
+  }
+}
+
+class RemoveTask extends DataEvent<Task> {
+  final url = Uri.parse("https://tutor-drp.herokuapp.com/deletetask");
+  late int _id;
+  late String _tuteeName;
+
+  RemoveTask(int id, String tuteeName) {
+    _id = id;
+    _tuteeName = tuteeName;
+  }
+
+  @override
+  Future<List<Task>> handle(List<String> fields) async {
+    final userID = fields[0];
+    await http.post(url, headers: {
+      "Cookie": "user_id=$userID",
+    }, body: {
+      "task_id": "$_id",
+    });
+    return RefreshTask(_tuteeName).handle(fields);
+  }
+}
+
+class AddTask extends DataEvent<Task> {
   final _format = DateFormat("yyyy-MM-ddThh:mm");
   final Uri url = Uri.parse("https://tutor-drp.herokuapp.com/addtask");
 
   late Task _task;
   late String _tuteeName;
 
-  Add(Task task, String tuteeName) {
+  AddTask(Task task, String tuteeName) {
     _task = task;
     _tuteeName = tuteeName;
   }
 
   @override
-  Future<List<Task>> handle(String userID, String tuteeID) async {
+  Future<List<Task>> handle(List<String> fields) async {
+    final userID = fields[0];
+    final tuteeID = fields[1];
     await http.post(url, headers: {
       "Cookie": "user_id=$userID",
     }, body: {
@@ -82,92 +143,10 @@ class Add extends TaskEvent {
       "content": _task.content,
       "tutee_id": tuteeID,
     });
-    return Refresh(_tuteeName).handle(userID, tuteeID);
+    return RefreshTask(_tuteeName).handle(fields);
   }
 
   String _formatTime(DateTime time) {
     return _format.format(time);
-  }
-}
-
-class Remove extends TaskEvent {
-  final url = Uri.parse("https://tutor-drp.herokuapp.com/deletetask");
-  late int _id;
-  late String _tuteeName;
-
-  Remove(int id, String tuteeName) {
-    _id = id;
-    _tuteeName = tuteeName;
-  }
-
-  @override
-  Future<List<Task>> handle(String userID, String tuteeID) async {
-    await http.post(url, headers: {
-      "Cookie": "user_id=$userID",
-    }, body: {
-      "task_id": "$_id",
-    });
-    return Refresh(_tuteeName).handle(userID, tuteeID);
-  }
-}
-
-class Refresh extends TaskEvent {
-  final url = Uri.parse("https://tutor-drp.herokuapp.com/tuteetasks");
-  late String? _tuteeName; //Needed to know which tutee to get data for
-
-  Refresh(String? tuteeName) {
-    this._tuteeName = tuteeName;
-  }
-
-  final DateFormat format = DateFormat("Y");
-
-  @override
-  Future<List<Task>> handle(String _, String tuteeID) async {
-    var resp = await http.get(url, headers: {
-      "Cookie":
-          "user_id=$tuteeID${_tuteeName == null ? "" : ";tutee_name=$_tuteeName"}"
-    });
-    if (resp.statusCode != 200) {
-      return List.empty();
-    }
-    var rawTasks = json.decode(resp.body);
-    var tasks = _parseTasks(rawTasks);
-    return tasks;
-  }
-
-  List<Task> _parseTasks(List raw) {
-    return raw.map((e) => _parseTask(e as Map)).toList();
-  }
-
-  Task _parseTask(Map obj) {
-    return Task(
-      _parseTime(obj["startTime"]!),
-      _parseTime(obj["endTime"]!),
-      obj["content"]!,
-      obj["done"]!,
-      obj["id"]!,
-    );
-  }
-
-  DateTime _parseTime(String timeString) {
-    return DateTime.parse(timeString);
-  }
-}
-
-class Done extends TaskEvent {
-  final url = Uri.parse("https://tutor-drp.herokuapp.com/donetask");
-  late int _id;
-
-  Done(int id) {
-    this._id = id;
-  }
-
-  Future<List<Task>> handle(String userID, String tuteeID) async {
-    await http.post(url, headers: {
-      "Cookie": "user_id=$userID",
-    }, body: {
-      "task_id": "$_id",
-    });
-    return Refresh("Mika").handle(userID, tuteeID);
   }
 }
