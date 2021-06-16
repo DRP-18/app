eec8e83b7880390c8614dfa4cac6da80e59449ea
Akshat Tripathi <akshat.tripathi6568@gmail.com>
2021-06-16 14:53:30 +0100

    Modified tuteeBloc to use DataBloc

--- tutor_me/lib/bloc/tutees.dart
+++ tutor_me/lib/bloc/tutees.dart
@@ -2,67 +2,16 @@ import 'dart:convert';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:bloc/bloc.dart';
 import 'package:http/http.dart' as http;
+import 'package:tutor_me/bloc/data_bloc.dart';
 
-class TuteeBloc extends Bloc<TuteeEvent, List<Tutee>> {
+class TuteeBloc extends DataBloc<Tutee> {
   final String userID;
 
-  TuteeBloc(this.userID) : super([]);
-
-  List<Tutee> get initialState => [];
+  TuteeBloc(this.userID) : super([userID]);
 
   @override
-  Stream<List<Tutee>> mapEventToState(TuteeEvent event) async* {
-    yield await event.handle(userID);
-  }
-}
-
-abstract class TuteeEvent {
-  Future<List<Tutee>> handle(String userID);
-}
-
-class Add extends TuteeEvent {
-  final Uri url = Uri.parse("https://tutor-drp.herokuapp.com/addtutee");
-  late String _tuteeName;
-
-  Add(String tuteeName) {
-    _tuteeName = tuteeName;
-  }
-
-  @override
-  Future<List<Tutee>> handle(String userID) async {
-    await http.post(url, headers: {
-      "Cookie": "user_id=$userID",
-    }, body: {
-      "tutee_name": _tuteeName,
-    });
-    return Refresh().handle(userID);
-  }
-}
-
-class Refresh extends TuteeEvent {
-  final Uri url = Uri.parse("https://tutor-drp.herokuapp.com/viewtutees");
-
-  @override
-  Future<List<Tutee>> handle(String userID) async {
-    var resp = await http.get(url, headers: {
-      "Cookie": "user_id=$userID",
-    });
-    if (resp.statusCode != 200) {
-      return List.empty();
-    }
-    var rawTutees = json.decode(utf8.decode(resp.bodyBytes));
-    return _parseTutees(rawTutees);
-  }
-
-  List<Tutee> _parseTutees(List raw) {
-    return raw.map((e) => _parseTutee(e as Map)).toList();
-  }
-
-  Tutee _parseTutee(Map obj) {
-    return Tutee(
-      obj["name"]!,
-      obj["id"]!,
-    );
+  Stream<List<Tutee>> mapEventToState(DataEvent<Tutee> event) async* {
+    yield await event.handle([userID]);
   }
 }
 
@@ -70,4 +19,40 @@ class Tutee {
   final String name;
   final int id;
   const Tutee(this.name, this.id);
+}
+
+class RefreshTutee extends Refresh<Tutee> {
+  final Uri url = Uri.parse("https://tutor-drp.herokuapp.com/viewtutees");
+
+  @override
+  Tutee dataConstructor(Map obj) => Tutee(
+        obj["name"]!,
+        obj["id"]!,
+      );
+
+  @override
+  Future<http.Response> request(List<String> fields) {
+    final userID = fields[0];
+    return http.get(url, headers: {
+      "Cookie": "user_id=$userID",
+    });
+  }
+}
+
+class AddTutee extends DataEvent<Tutee> {
+  final Uri url = Uri.parse("https://tutor-drp.herokuapp.com/addtutee");
+  final String _tuteeName;
+
+  AddTutee(this._tuteeName);
+
+  @override
+  Future<List<Tutee>> handle(List<String> fields) async {
+    final userID = fields[0];
+    await http.post(url, headers: {
+      "Cookie": "user_id=$userID",
+    }, body: {
+      "tutee_name": _tuteeName,
+    });
+    return RefreshTutee().handle(fields);
+  }
 }
